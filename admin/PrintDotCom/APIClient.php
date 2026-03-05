5180bf5c3b08af067074a0b7a48d6faf13fb0f67
tijmen <tijmenbruggeman@gmail.com>
2026-03-05 10:46:01 +0100

    fix: sort presets alphabetically

--- admin/PrintDotCom/APIClient.php
+++ admin/PrintDotCom/APIClient.php
@@ -193,6 +193,12 @@ class APIClient {
 				return $preset->sku === $sku;
 			}
 		);
+
+		usort(
+			$filtered_by_sku,
+			fn( $a, $b ) => strcasecmp( $a->title, $b->title )
+		);
+
 		return array_values( $filtered_by_sku );
 	}
 
